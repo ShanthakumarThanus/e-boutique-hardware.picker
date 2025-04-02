d7a94a8e2128ad775f89084331671e205af40baf
ShanthakumarThanus <thanus.shanthakumar.sio@gmail.com>
2025-04-02 21:01:02 +0200

    ajout page d'accueil

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -34,8 +34,7 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // do anything else you need here, like send an email
-
-            return $security->login($user, 'form_login', 'main');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
